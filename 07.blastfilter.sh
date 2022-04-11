3d9aa3f8241a6c3065b6fd24bf49116633069e6c
gitzl222 <90139726+gitzl222@users.noreply.github.com>
2022-04-11 10:34:02 +0800

    Update 07.blastfilter.sh

--- 07.blastfilter.sh
+++ 07.blastfilter.sh
@@ -1,4 +1,4 @@
 sample=$1
 input_dir=$2
 
-python /media/bacteria/B6260EBD260E7F1D/nanopore/db/python/blast.filter.py ${input_dir}/trimed/${sample}/${sample}.blast.txt /media/bacteria/B6260EBD260E7F1D/nanopore/db/refseq_all2/all.name.txt ${input_dir}/trimed/${sample}/${sample}_py.blast.txt
+python /blast.filter.py ${input_dir}/trimed/${sample}/${sample}.blast.txt /refseq/species.name.txt ${input_dir}/trimed/${sample}/${sample}_py.blast.txt
